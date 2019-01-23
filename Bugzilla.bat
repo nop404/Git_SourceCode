26c3f64ad757f2261ab14ebc1af6867d2e5e7002
nop404 <nop404@qq.com>
2019-01-23 19:53:57 +0800

    增加网址收藏

---
+++ Bugzilla.bat
@@ -0,0 +1,5 @@
+https://github.com/bugzilla/bugzilla.git
+https://github.com/bugzilla/harmony.git
+https://github.com/bugzilla/bugzilla.github.io.git
+https://github.com/bugzilla/harmony-test-fixtures.git
+https://github.com/bugzilla/harmony-systems.git
